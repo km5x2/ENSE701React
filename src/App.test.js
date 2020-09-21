ee3979d67500d5984aa578f409190b47cf7fd2f1
km5x2 <kenneth.s97@gmail.com>
2020-09-21 14:37:39 +1200

    change test param

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/LINKS/i);
+  const linkElement = getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
 
